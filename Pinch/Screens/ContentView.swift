200dd9227d0b398792e8225ac9c4d085d4702be7
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-04-22 19:54:17 -0300

    fading image loading at app load

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -9,18 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: PROPERTIES
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
         NavigationView{
             ZStack {
+                // MARK: PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
         }.navigationViewStyle(.stack)
     }
 }
